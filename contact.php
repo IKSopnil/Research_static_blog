a9888f741fec5cfcce657d2115727f38c2caa61f
IKSopnil <sopnil4o4o@gmail.com>
2023-05-02 09:48:57 +0600

    contact associate

---
+++ contact.php
@@ -0,0 +1,12 @@
+<head>
+
+<!-- Load Bootstrap CSS from CDN -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">
+
+<!-- Load custom CSS -->
+<link rel="stylesheet" href="style.css">
+</head>
+
+<?php 
+include "navbar.php";
+?>
