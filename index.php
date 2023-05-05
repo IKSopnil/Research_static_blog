15fb4ce6a131566b93faaaa6bf10d23867b551a2
IKSopnil <sopnil4o4o@gmail.com>
2023-05-05 12:38:26 +0600

    seo

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top 30 Thesis, Research Topic in Computer Science - Best Topics</title>
     <meta name="description" content="Find the best topics for your thesis or research project in computer science. Get inspired by our list of the top 30 topics, including AI, machine learning, cybersecurity, and more.">
-    <meta name="keywords" content="top 30, thesis, research, computer science, AI, machine learning, cybersecurity">
+    <meta name="keywords" content="computer science, AI, machine learning, cybersecurity">
     <link rel="shortcut icon" href="pc.png">
     
     <!-- Add Open Graph tags for social media sharing -->
