35a064f5435a16076749218b23f6aea5ca84ed6a
IKSopnil <sopnil4o4o@gmail.com>
2023-05-11 15:30:43 +0600

    m

--- index.php
+++ index.php
@@ -163,32 +163,8 @@
             });
         });
     </script>
-  
-<section>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark py-4">
-            <a class="navbar-brand ps-5" href="index.php">
-                <p>Thesis and Research</p>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-center pe-5 me-5" style="font-weight: bold;"
-                id="navbarNav">
-                <ul class="navbar-nav ">
-                    <li class="nav-item mx-4">
-                        <a class="nav-link" href="index.php">Home</a>
-                    </li>
-                    <li class="nav-item mx-4">
-                        <a class="nav-link" href="about.php">About</a>
-                    </li>
-                    <li class="nav-item mx-4">
-                        <a class="nav-link" href="contact.php">Contact</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </section>    <section class="container mt-5 ">
+<?php include 'navbar.php'?>  
+<section class="container mt-5 ">
         <div>
             <h1 style="font-weight: bold;">Top 30 Thesis and Research topic for Computer Science. </h1>
         </div>
